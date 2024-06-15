e95dc385dce7b589c9f89c335a0a88d8a452ceeb
cschwarz007 <christian_schwarz@gmx.de>
2024-06-15 16:04:09 +0200

    Update rdm.R

--- R/rdm.R
+++ R/rdm.R
@@ -1,11 +1,17 @@
 rdm <-
-function(n, a=1, zr=0.5, v=1, t0=0.25, d=0, szr=0, sv=0, st0=0, N=1, wd_fastdm=path, removeTempFiles=T){
+function(n, a=1, zr=0.5, v=1, t0=0.25, d=0, szr=0, sv=0, st0=0, N=1, data_path = "", removeTempFiles=T){
   if(a <= 0 | zr >= 1 | zr <= 0 | szr < 0 | sv < 0 | st0 < 0){
     stop("invalid parameter manifestations")
   }else if(N <= 0 | n <= 0) stop("number of replicates and samples must be >0")
-  wd_temp <- getwd(); setwd(wd_fastdm) #change working directory to fast-dm
+  wd_fastdm = paste0(find.package("fastRdm"),"/fastdm_30_2")
+  if(data_path=="") {
+    data_path = wd_fastdm
+  } else {
+    dir.create(data_path)
+  }    
+  wd_temp <- getwd(); setwd(data_path) #change working directory to fast-dm
   call_args <- paste("-a",a,"-z",zr,"-v",v,"-t",t0,"-d",d,"-Z",szr,"-V",sv,"-T",st0,"-r","-n",n,"-N",N)
-  system(paste("construct-samples",call_args,'-o "%d.rdm"'), show.output.on.console=FALSE)
+  system(paste(paste0(wd_fastdm,"/",get_os_binary("construct-samples")),call_args,'-o "%d.rdm"'), show.output.on.console=FALSE)
   if(N == 1){
     out <- read.table("0.rdm", header = FALSE)
     names(out) <- c("RESPONSE","TIME")
